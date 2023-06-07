8504042a65efd696737b3b17dd510f5da5a34dfd
David Xiao <davexiao565@gmail.com>
2023-06-07 13:48:46 -0500

    VERSION 3.1 - Iteration One for (NUM_GREATER_MOD)

---
+++ Num_Greater_Mod.py
@@ -0,0 +1,12 @@
+numA = 922
+numB = 934
+numC = 344
+
+
+def findnummod(num):
+    return num >= 4 ** 4 and num % 34 == 4
+
+
+print(findnummod(numA))
+print(findnummod(numB))
+print(findnummod(numC))
