8d0d0bcdf4804117a51d9a1ce24445ea63456b5b
David Xiao <davexiao565@gmail.com>
2023-06-14 10:05:51 -0500

    VERSION 9.2 - Iteration Two for (NUM_CONSECUTIVE)

--- Num_Consecutive.py
+++ Num_Consecutive.py
@@ -1,19 +1,17 @@
 listA = [1, 2, 3, 4, 5, 6, 7]
 listB = [1, 2, 3, 3, 4, 4, 3]
 listC = [1, 2, 3, 4, 3, 2, 1]
+listD = [1, 11, 1, 11, 1, 11]
 
 
 def findconsecutive(used_list):
-    cnum = used_list[0]
-    for num in used_list[1:-1]:
-        if cnum != num:
-            cnum = num
-        else:
+    for num in range(len(used_list)-1):
+        if used_list[num] == used_list[num+1]:
             return False
-    if cnum != used_list[-1]:
-        return True
+    return True
 
 
 print(findconsecutive(listA))
 print(findconsecutive(listB))
 print(findconsecutive(listC))
+print(findconsecutive(listD))
